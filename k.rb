f306811da28004dca97d738fa3d552fb5f893500
niugrace <grace.niu.01@gmail.com>
2018-07-13 19:51:38 +0000

    automatically backed up by learn

---
+++ k.rb
@@ -0,0 +1,23 @@
+class Dog 
+  def initialize(name, breed)
+    @name = name
+    @breed = breed 
+  end 
+  
+  public 
+  def bark 
+    puts "Woof!"
+  end 
+  
+  private 
+  def id 
+    @id number = 12345
+  end
+end 
+
+
+
+
+
+
+
